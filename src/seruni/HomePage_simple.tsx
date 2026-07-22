0b6e84f19eba75d4fb30df66a8ac0de5266a25ed
Seruni Mumbul <serunimumbul2010@gmail.com>
2026-07-22 12:48:39 +0700

    Restore HomePage complex

---
+++ src/seruni/HomePage_simple.tsx
@@ -0,0 +1,8 @@
+export default function HomePage() {
+  return (
+    <div className="min-h-screen bg-gray-50">
+      <h1 className="text-4xl font-bold p-8 text-center">Portal Desa Seruni Mumbul</h1>
+      <p className="text-center text-gray-600">Website sedang dimuat...</p>
+    </div>
+  );
+}
